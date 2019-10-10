6989b42c63b6bddfeaae13c184abf038341b36fe
lamducanhndgv <lamducanhndgv@gmail.com>
2019-10-11 00:31:22 +0700

    Convert to OOP style , create Polygon and Coordinate

--- Project01.py
+++ Project01.py
@@ -1,13 +1,8 @@
 import matplotlib.pyplot as plt
 from matplotlib import colors
 import numpy as np
-
-
-class Coordinate:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
+from Coordinate import Coordinate
+from Polygon import Polygon
 
 wdir = 'input.txt'
 
@@ -18,7 +13,7 @@ wdir = 'input.txt'
 """
 
 
-def readInput(filepath):
+def read_input(filepath):
     result = []
     count = 0
     with open(filepath) as fp:
@@ -26,15 +21,15 @@ def readInput(filepath):
         while line:
             line = line.replace('\n', '')
             line = line.replace(' ', '')
-            listCoord = line.split(',')
-            size = len(listCoord)
+            list_coord = line.split(',')
+            size = len(list_coord)
             if size == 1:
                 line = fp.readline()
                 continue
-            result.append([])
-            for x in range(0, len(listCoord)):
+            result.append(Polygon())
+            for x in range(0, len(list_coord)):
                 if x + 1 < size and x % 2 == 0:
-                    result[count].append(Coordinate(int(listCoord[x + 1]), int(listCoord[x])))
+                    result[count].append_point(Coordinate(int(list_coord[x + 1]), int(list_coord[x])))
             count += 1
 
             line = fp.readline()
@@ -52,14 +47,14 @@ def makeDataSet(dataCoord):
     data = np.ones((heigh, width)) * np.nan
 
     for i in range(1, len(dataCoord)):
-        size = len(dataCoord[i]);
+        size = dataCoord[i].size if isinstance(dataCoord[i], Polygon) else len(dataCoord[i].size)
         for j in range(size):
             data[dataCoord[i][j].x][dataCoord[i][j].y] = int(i)
-        if (i > 1):
-            min_x, min_y, max_x, max_y = findRectangle(dataCoord[i])
+        if i > 1:
+            min_x, min_y, max_x, max_y = dataCoord[i].find_rectangle()
             for x in range(min_x, max_x + 1):
                 for y in range(min_y, max_y + 1):
-                    if (point_inside_polygon(x, y, dataCoord[i]) == True):
+                    if dataCoord[i].contain_point(x, y):
                         data[x][y] = i
             for j in range(size - 1):
                 plotLine(data, dataCoord[i][j], dataCoord[i][j + 1], i)
@@ -130,75 +125,35 @@ def plotLineHigh(data, fromCoord, toCoord, index):
 
 
 """
-    Decide a given coordinate is inside a polygon
-    Source: http://www.ariel.com.au/a/python-point-int-poly.html
-"""
-
-
-def point_inside_polygon(x, y, poly):
-    n = len(poly)
-    inside = False
-
-    p1x, p1y = poly[0].x, poly[0].y
-    for i in range(n + 1):
-        p2x, p2y = poly[i % n].x, poly[i % n].y
-        if y > min(p1y, p2y):
-            if y <= max(p1y, p2y):
-                if x <= max(p1x, p2x):
-                    if p1y != p2y:
-                        xinters = (y - p1y) * (p2x - p1x) / float((p2y - p1y)) + p1x
-                    if p1x == p2x or x <= xinters:
-                        inside = not inside
-        p1x, p1y = p2x, p2y
-    return inside
-
-
-"""
-    find the bottom-left point and top-right point
-    
-"""
-
-
-def findRectangle(poly):
-    listX = []
-    listY = []
-    for i in range(len(poly)):
-        listX.append(poly[i].x)
-        listY.append(poly[i].y)
-    return min(listX), min(listY), max(listX), max(listY)
-
-
-#################################################################
-"""
     test ket qua
 """
 
-dataRead = readInput(wdir)
+if __name__ == "__main__":
+    dataRead = read_input(wdir)
+    width, heigh, data = makeDataSet(dataRead)
 
-width, heigh, data = makeDataSet(dataRead)
+    fig, ax = plt.subplots(1, 1, tight_layout=False)
 
-fig, ax = plt.subplots(1, 1, tight_layout=False)
+    my_cmap = colors.ListedColormap(['r', 'b', 'y', 'g', 'c'])
 
-my_cmap = colors.ListedColormap(['r', 'b', 'y', 'g', 'c'])
+    my_cmap.set_bad(color='w', alpha=0)
 
-my_cmap.set_bad(color='w', alpha=0)
+    bounds = [0, 1, 2, 3, 4, 5, 6]
 
-bounds = [0, 1, 2, 3, 4, 5, 6]
+    norm = colors.BoundaryNorm(bounds, my_cmap.N)
 
-norm = colors.BoundaryNorm(bounds, my_cmap.N)
+    # draw the grid
+    for x in range(heigh + 1):
+        ax.axhline(x, lw=1, color='k')
 
-# draw the grid
-for x in range(heigh + 1):
-    ax.axhline(x, lw=1, color='k')
+    for x in range(width + 1):
+        ax.axvline(x, lw=1, color='k')
 
-for x in range(width + 1):
-    ax.axvline(x, lw=1, color='k')
+    ax.set_xticks(np.arange(0, width + 1, 1))
+    ax.set_yticks(np.arange(0, heigh + 1, 1))
+    ax.grid(which='both')
 
-ax.set_xticks(np.arange(0, width + 1, 1))
-ax.set_yticks(np.arange(0, heigh + 1, 1))
-ax.grid(which='both')
+    ax.imshow(data, interpolation='none', cmap=my_cmap, extent=[0, width, 0, heigh], origin='lower', norm=norm)
 
-ax.imshow(data, interpolation='none', cmap=my_cmap, extent=[0, width, 0, heigh], origin='lower', norm=norm)
-
-plt.gcf().set_size_inches((10, 10))
-plt.show()
+    plt.gcf().set_size_inches((10, 10))
+    plt.show()
