fd4c8b30aaf4274792aba98f96622bf24f5e2741
Cong Hoang <hoangduccong85@gmail.com>
2019-10-19 15:29:49 +0700

    Complete animation

--- Project01.py
+++ Project01.py
@@ -477,7 +477,9 @@ def updateMat(data, Polygon, action,idx):
                     else: data[i][j]=math.nan
                 else: data[i][j]=math.nan
         for j in range(min_y, max_y+1):
-                data[max_x][j]= math.nan
+            if(not math.isnan(data[max_x][j])):
+                if(data[max_x][j]==idx):
+                    data[max_x][j]= math.nan
     elif (action == 1): # right
         for i in range(max_x+1, min_x,-1):
             for j in range(min_y, max_y+1):
@@ -487,7 +489,9 @@ def updateMat(data, Polygon, action,idx):
                     else: data[i][j]= math.nan
                  else: data[i][j]=math.nan
         for j in range(min_y, max_y+1):
-                data[min_x][j]= math.nan
+            if(not math.isnan(data[min_x][j])):
+                if(data[min_x][j]==idx):
+                    data[min_x][j]= math.nan
     elif (action == 2): # down
         for i in range(min_y-1,max_y):
             for j in range(min_x, max_x+1):
@@ -497,7 +501,9 @@ def updateMat(data, Polygon, action,idx):
                     else: data[j][i]= math.nan
                  else: data[j][i]=math.nan
         for j in range(min_x, max_x+1):
-                data[j][max_y]= np.nan
+            if(not math.isnan(data[j][max_y])):
+                if(data[j][max_y]==idx):
+                    data[j][max_y]= np.nan
     else: # up
         for i in range(max_y+1, min_y,-1):
             for j in range(min_x, max_x+1):
@@ -507,7 +513,9 @@ def updateMat(data, Polygon, action,idx):
                     else: data[j][i]= math.nan
                 else: data[j][i]=math.nan
         for j in range(min_x, max_x+1):
-                data[j][min_y]= math.nan
+            if(not math.isnan(data[j][min_y])):
+                if(data[j][min_y]==idx):
+                    data[j][min_y]= math.nan
 #    return data
     
 # tuong duong generate data
